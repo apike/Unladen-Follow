6fc52a662d8f07ae5286773eaecb389af8f46f8b
Allen Pike <ajpike@gmail.com>
2014-01-15 23:01:59 -0800

    Started node rewrite of Unladen Follow.

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  'HOSTPATH': 'http://your.host.here',
+  'PORT': 80,
+  'EXPRESS_SESSION_SECRET': '123456',
+  'TWITTER_CONSUMER_KEY': 'your-consumer-key-here',
+  'TWITTER_CONSUMER_SECRET': 'your-secret-here',
+  'GOOGLE_APP_ID': 'your-app-id-here',
+  'GOOGLE_CONSUMER_SECRET': 'your-consumer-secret-here',
+};
